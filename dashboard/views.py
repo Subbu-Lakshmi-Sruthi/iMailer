b9528d45d4e426b43230b55351558c96d9cff81b
Gowtham <sgowthamcoimbatore2@gmail.com>
2023-03-21 14:52:18 +0530

    authentication added

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse , redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
+
 
 # Create your views here.
+@login_required(login_url='/login')
 def dashboard(request):
     return render(request, "dashboard/main.html",{})
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('dashboard')
+    return render(request, "dashboard/signin.html",{})  
