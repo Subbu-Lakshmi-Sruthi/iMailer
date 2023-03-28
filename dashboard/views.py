0e889e6101a7251c81aedeb88bc17a13c2b4437b
Gowtham <sgowthamcoimbatore2@gmail.com>
2023-03-28 21:27:47 +0530

    spam word detection

--- dashboard/views.py
+++ dashboard/views.py
@@ -5,6 +5,11 @@ from django.shortcuts import render, HttpResponse , redirect
 from .forms import *
 from .models import *
 import json
+import openai
+
+
+
+
 
 # Create your views here.
 @login_required(login_url="login")
@@ -146,3 +151,27 @@ def send_mail_bulk(request):
     form = MailForm()
     return render(request, "dashboard/sendmail_bulk.html",{"send_mail_bulk_active": True, "form":form, 'templates' : templates,
         'public_templates': public_templates,})
+
+
+openai.api_key = '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'
+
+def chatbot_response(user_input):
+# Generate a response from GPT-3
+    response = openai.Completion.create(
+        engine='text-davinci-002',
+        prompt=user_input,
+        temperature=0.5,
+        max_tokens=1024,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    return response['choices'][0]['text']
+    
+def ChatbotView(request):
+    if request.method == "POST":
+        user_input = request.POST['user_input']
+        response = chatbot_response(user_input)
+        return render(request , 'dashboard/curiator.html' , {'response' : response})
+    
+    return render(request , 'dashboard/curiator.html')
